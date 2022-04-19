1df5b244357090287da9d5d856b98fbc653eaea3
Kanhaiya <kanhaiya.choudhary1@gmail.com>
2022-04-19 18:40:26 +0530

    Revert "Added sum logic"
    This reverts commit 8d19fbf24cef8540dbe15ab2d20bb90ba8c9b610.

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,1 @@
 print("adding all the data processing code related to the fraud detection")
-
-a = 5
-b = 10
-
-def sum(a, b):
-    return a - b
-
-print ('Added a new function sum which is not correct')
